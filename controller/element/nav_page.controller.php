393bb3009954e04eb822a88c94398a4b66d658c6
Marius Mandal <mariusmandal@gmail.com>
2013-11-22 22:43:21 +0100

    page menu content

--- controller/element/nav_page.controller.php
+++ controller/element/nav_page.controller.php
@@ -6,32 +6,32 @@ switch( get_option('homepage_nav') ) {
 	## MENU OF UKM.no FRONTPAGE
 	default: 
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring1',
+											   'title'		 	=> 'Finn din mønstring',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring2',
+											   'title'		 	=> 'Hva er UKM?',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring3',
+											   'title'		 	=> 'Alt om URG',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring4',
+											   'title'		 	=> 'Tidligere nettsider',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring5',
+											   'title'		 	=> 'UKM-TV',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
 		$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-											   'title'		 	=> 'Finn din mønstring6',
+											   'title'		 	=> 'Whatevva',
 											   'icon'			=> 'mobile',
 											   'description'	=> 'UKM består av én lokalmønstring for hver kommune. Finn din her.'
 											  );
