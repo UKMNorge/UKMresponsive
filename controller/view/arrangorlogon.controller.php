974f5b15e49db88a5b88387de92572dd6c55b939
Daniel Skogstad <daniel.skogstad@gmail.com>
2013-11-23 15:58:54 +0100

    cleaning up

--- controller/view/arrangorlogon.controller.php
+++ controller/view/arrangorlogon.controller.php
@@ -1,8 +1,10 @@
 <?php
+require_once('UKM/sql.class.php');
 
 class ArrangorLogonController {
     
     protected $version;
+    protected $cookies;
     
     public function __construct()
     {
@@ -12,28 +14,86 @@ class ArrangorLogonController {
         
         $this->version = get_option('season');
         
-        
+        $this->cookies = array(
+            'UKM_username', 'UKM_email',
+            'UKM_kommune', 'UKM_pl_id',
+            'UKM_pl_type', 'UKM_pl_fylke',
+            'UKM' . $this->version . '_username',
+            'UKM' . $this->version . '_email',
+            'UKM' . $this->version . '_kommune',
+            'UKM' . $this->version . '_pl_id',
+            'UKM' . $this->version . '_pl_type',
+            'UKM' . $this->version . '_pl_fylke'
+        );
     }
     
-    public function login()
+    public function renderAction()
     {
+        if(isset($_GET['logout'])) {
+            $this->logout();
+        }
+        
+        if(!empty($_POST['ukm_user']) && !empty($_POST['ukm_pass'])) {
+            $user = $_POST['ukm_user'];
+            $pass = $_POST['ukm_pass'];
+            
+            $this->login($user, $pass);
+        }
+    }
+    
+    public function login($user, $pass)
+    {
+        global $wpdb;
+        
+        $wpdb->query(
+            $wpdb->prepare(
+                'SELECT '.
+                '`b`.`b_name` AS `username`, ' .
+                '`b`.`b_email` AS `email`, ' .
+                '`b`.`b_kommune` AS `kommune`, ' .
+                '`b`.`b_fylke` AS `pl_fylke` ' .
+                'FROM `ukm_brukere` AS `b` ' .
+                'WHERE `b_name` = %s ' .
+                'AND `b_password` = %s ',
+                $user, $pass
+            )
+        );
+        
+        $query = 'SELECT '.
+                 '`b`.`b_name` AS `username`, ' .
+				 '`b`.`b_email` AS `email`, ' .
+				 '`b`.`b_kommune` AS `kommune`, ' .
+				 '`b`.`b_fylke` AS `pl_fylke` ' .
+				 'FROM `ukm_brukere` AS `b` ' .
+				 'WHERE `b_name` = "#user" ' .
+				 'AND `b_password` = "#pass" ';
+				 
+		
+		$res = $wpdb->query($qry);
+        
+        $sql = new SQL($query);
+        
+        $result = $sql->run('array');
+        
         
     }
     
     public function logout()
     {
-        
+        $this->setCookie($this->cookies);
+        $this->unsetCookie($this->cookies);
+        $this->redirect("Location: " . get_permalink() . '?UKMstatus=logget_ut');
     }
     
-    public function setCookie($cookie) 
+    public function setCookie($cookie, $value = '') 
     {
         if(is_array($cookie)) {
             foreach($cookie as $c) {
-                setcookie($c, '', 1);
+                setcookie($c, $value, 1);
             }
         }
         else {
-            setcookie($cookie, '', 1);
+            setcookie($cookie, $value, 1);
         }
     }
     
@@ -47,6 +107,12 @@ class ArrangorLogonController {
         else {
             unset($_COOKIE[$c]);
         }
+    }
+    
+    public function redirect($url)
+    {
+        header("location: " . $url);
+        die();
     }
     
     
