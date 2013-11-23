a3231d5be89612dcb53b0e24de37134f69b859f2
svenove <git@sven-ove.no>
2013-11-23 15:52:29 +0100

    Icons

--- controller/view/kommune.controller.php
+++ controller/view/kommune.controller.php
@@ -4,59 +4,62 @@ require_once('UKM/monstring.class.php');
 $DATA['kommune'] = get_option('kommune');
 
 // HENT ALLE POSTS
-	$DATA['posts'] = array();
-	
-	// LOAD PAGE DATA
-	the_post();
-	$DATA['page'] = new WPOO_Post( $post );
-	
-	// LOAD POSTS
-	$posts_array = get_posts( 'posts_per_page=6' );
-	foreach( $posts_array as $post ) {
-		$DATA['posts'][] = new WPOO_Post( $post );
-	}
-	
+        $DATA['posts'] = array();
+
+        // LOAD PAGE DATA
+        the_post();
+        $DATA['page'] = new WPOO_Post( $post );
+
+        // LOAD POSTS
+        $posts_array = get_posts( 'posts_per_page=6' );
+        foreach( $posts_array as $post ) {
+                $DATA['posts'][] = new WPOO_Post( $post );
+        }
+
 // INFO OM MØNSTRINGEN
-	$pl = new monstring( get_option('pl_id') );
-	$monstring = new StdClass();
-	$monstring->starter = $pl->get('pl_start');
-	$monstring->slutter = $pl->get('pl_stop');
-	$monstring->sted = $pl->get('pl_place');
-	$DATA['monstring'] = $monstring;
+        $pl = new monstring( get_option('pl_id') );
+        $monstring = new StdClass();
+        $monstring->starter = $pl->get('pl_start');
+        $monstring->slutter = $pl->get('pl_stop');
+        $monstring->sted = $pl->get('pl_place');
+        $DATA['monstring'] = $monstring;
 
 // HVILKEN PERIODE ER KOMMUNESIDEN I?      
         if (!$m->registered())
             $VIEW = 'kommune_ikke_klar';
-        
+
         else {
             $utenforsesong = mktime(0,0,0,12,1,get_option('season')-1)>time();
             if ($utenforsesong) {
                 $VIEW = 'kommune_pre_lokal';
             } else if ( time() < $m->frist()) {
-                    $VIEW = 'kommune_lokal';
+                $VIEW = 'kommune_lokal';
             } elseif( time() > $m->frist() && time() < $pl->get('pl_start') ) {
-                    $VIEW = 'kommune_pre';
+                $VIEW = 'kommune_pre';
             } elseif( time() > $pl->get('pl_start') && time() < $pl->get('pl_stop') ) {
-                    $VIEW = 'kommune';
+                $VIEW = 'kommune';
             } elseif( time() > $pl->get('pl_stop') ) {
-                    $VIEW = 'kommune_post';
+                $VIEW = 'kommune_post';
             }
         }
-            
+        
+        $DATA = array_merge($monstring->pameldingsikoner());
+
 // PAGE NAV
-	$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-										   'title'		 	=> 'Program',
-										   'icon'			=> 'mobile',
-										   'description'	=> 'Se program for lokalmønstringen'
-										  );
-	$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-										   'title'		 	=> 'Hvem deltar?',
-										   'icon'			=> 'mobile',
-										   'description'	=> 'Se alle som deltar på lokalmønstringen.'
-										  );
-	$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
-										   'title'		 	=> 'Kontaktpersoner',
-										   'icon'			=> 'mobile',
-										   'description'	=> 'Har du spørsmål om UKM i '. $pl->get('pl_name').'? Disse kan hjelpe!'
-										  );
+        $DATA['page_nav'][] = (object) array( 'url' => '#',
+                                            'title' => 'Program',
+                                            'icon'  => 'mobile',
+                                            'description' => 'Se program for lokalmønstringen'
+                                           );
+        $DATA['page_nav'][] = (object) array( 'url' => '#',
+                                              'title' => 'Hvem deltar?',
+                                              'icon' => 'mobile',
+                                              'description' => 'Se alle som deltar på lokalmønstringen.'
+                                          );
+        $DATA['page_nav'][] = (object) array( 'url' => '#',
+                                              'title' => 'Kontaktpersoner',
+                                              'icon'  => 'mobile',
+                                              'description' => 'Har du spørsmål om UKM i '. $pl->get('pl_name').'? Disse kan hjelpe!'
+                                          );
 	
+?>
