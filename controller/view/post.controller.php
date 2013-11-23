3f2a8c47e623398d1310a740d5fc8407c5e665f0
Daniel Skogstad <daniel.skogstad@gmail.com>
2013-11-23 19:29:42 +0100

    arrangorlogon controller + view + removed hardcoded picture in post controller

--- controller/view/post.controller.php
+++ controller/view/post.controller.php
@@ -3,7 +3,6 @@
 the_post();
 $DATA['post'] = new WPOO_Post( $post );
 
-$DATA['post']->author->image = 'http://placehold.it/85x64';
 $DATA['post']->author->company_name = 'UKM Norge';
 
 /* TODO: Definere:
