98a1c05b50af0ad53e034730e67087a954447889
Daniel Skogstad <daniel.skogstad@gmail.com>
2013-11-23 22:17:36 +0100

    added pagination to fylke and kommune

--- controller/view/fylke.controller.php
+++ controller/view/fylke.controller.php
@@ -11,10 +11,21 @@ $DATA['fylke'] = get_option('fylke');
 	$DATA['page'] = new WPOO_Post( $post );
 	
 	// LOAD POSTS
-    $posts = query_posts('posts_per_page=6');
+    $paged = (get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
+    
+    $posts = query_posts('posts_per_page=6&paged='.$paged);
     while(have_posts()) {
        the_post();
        $DATA['posts'][] = new WPOO_Post($post); 
+    }
+    
+    $npl = explode('"',get_next_posts_link()); 
+    $DATA['nextpost']=$npl[1];
+    
+    $ppl = get_previous_posts_link();
+    if($ppl) {
+        $ppl = explode('"',$ppl); 
+        $DATA['prevpost']=$ppl[1];
     }
 	
 // INFO OM MØNSTRINGEN
--- controller/view/homepage.controller.php
+++ controller/view/homepage.controller.php
@@ -5,13 +5,23 @@ $DATA['posts'] = array();
 the_post();
 $DATA['page'] = new WPOO_Post( $post );
 
+$paged = (get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
 
-$posts = query_posts('posts_per_page=6');
+$posts = query_posts('posts_per_page=6&paged='.$paged);
 
 while(have_posts()) {
    the_post();
    $DATA['posts'][] = new WPOO_Post($post); 
-} 
+}
 
+$npl = explode('"',get_next_posts_link()); 
+$DATA['nextpost']=$npl[1];
+
+$ppl = get_previous_posts_link();
+if($ppl) {
+    $ppl = explode('"',$ppl); 
+    $DATA['prevpost']=$ppl[1];
+}
+wp_reset_postdata(); 
 
 require_once(THEME_PATH . 'controller/element/nav_page.controller.php');
