38cf7fdaef2569d793759e8c920f544f3dbcd905
Jørgen Hagen Bendiksen <jhbendiksen@gmail.com>
2013-11-23 12:33:08 +0100

    birth of post byline element

--- controller/view/post.controller.php
+++ controller/view/post.controller.php
@@ -3,7 +3,11 @@
 the_post();
 $DATA['post'] = new WPOO_Post( $post );
 
+$DATA['post']->author->image = 'http://placehold.it/85x64';
+$DATA['post']->author->company_name = 'UKM Norge';
+
 /* TODO: Definere:
+    post.author.image
     post.author.company_name
     post.author.facebook_url
 */
