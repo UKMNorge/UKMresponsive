7a0f1f1dedee945016c68fc9338d2b2cda0a0af3
Marius Mandal <mariusmandal@gmail.com>
2013-11-23 11:15:00 +0100

    Fylke-kontroller

--- controller/view/fylke.controller.php
+++ controller/view/fylke.controller.php
@@ -1,2 +1,84 @@
 <?php
+require_once('UKM/monstring.class.php');
+
 $DATA['fylke'] = get_option('fylke');
+
+// HENT ALLE POSTS
+	$DATA['posts'] = array();
+	
+	// LOAD PAGE DATA
+	the_post();
+	$DATA['page'] = new WPOO_Post( $post );
+	
+	// LOAD POSTS
+	$posts_array = get_posts( 'posts_per_page=6' );
+	foreach( $posts_array as $post ) {
+		$DATA['posts'][] = new WPOO_Post( $post );
+	}
+	
+// INFO OM MØNSTRINGEN
+$pl = new monstring( get_option('pl_id') );
+$monstring = new StdClass();
+$monstring->starter = $pl->starter();
+$monstring->slutter = $pl->slutter();
+$DATA['monstring'] = $monstring;
+
+// INFO OM LOKALMØNSTRINGER
+$lokal_plids = $pl->hent_lokalmonstringer();
+$forste = 0;
+$siste = 0;
+$siste_pamelding = 0;
+
+foreach( $lokal_plids as $lm ) {
+	$lokalmonstring = new monstring( $lm );
+	
+	if($forste == 0) {
+		$forste = $lokalmonstring->get('pl_start');
+		$siste = $lokalmonstring->get('pl_stop');
+	}
+	
+	if( $lokalmonstring->get('pl_start') <  $forste ) {
+		$forste = $lokalmonstring->get('pl_start');
+	}
+	
+	if( $lokalmonstring->get('pl_stop') > $siste ) {
+		$siste = $lokalmonstring->get('pl_stop');
+	}
+	
+	if( $lokalmonstring->get('pl_deadline') > $siste_pamelding ) {
+		$siste_pamelding = $lokalmonstring->get('pl_deadline');
+	}
+	
+	$lokalmonstringer[] = $lokalmonstring;
+}
+$DATA['lokalmonstringer'] = $lokalmonstringer;
+
+
+// HVILKEN PERIODE ER FYLKESSIDEN I?
+$VIEW = 'fylke_pre_lokal';
+if( time() > $siste_pamelding && time() < $siste ) {
+	$VIEW = 'fylke_lokal';
+} elseif( time() > $siste && time() < $pl->get('pl_start') ) {
+	$VIEW = 'fylke_pre';
+} elseif( time() > $pl->get('pl_start') && time() < $pl->get('pl_stop') ) {
+	$VIEW = 'fylke';
+} elseif( time() > $pl->get('pl_stop') ) {
+	$VIEW = 'fylke_post';
+}
+
+// PAGE NAV
+$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
+									   'title'		 	=> 'Program',
+									   'icon'			=> 'mobile',
+									   'description'	=> 'Se program for fylkesmønstringen'
+									  );
+$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
+									   'title'		 	=> 'Hvem deltar?',
+									   'icon'			=> 'mobile',
+									   'description'	=> 'Se alle som deltar på fylkesmønstringen.'
+									  );
+$DATA['page_nav'][] = (object) array( 'url' 			=> '#',
+									   'title'		 	=> 'Kontaktpersoner',
+									   'icon'			=> 'mobile',
+									   'description'	=> 'Har du spørsmål om UKM i '. $pl->get('pl_name').'? Disse kan hjelpe!'
+									  );
