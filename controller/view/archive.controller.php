1cb4f0af3633b82130390f9ec256e89c91e3dfe0
Daniel Skogstad <daniel.skogstad@gmail.com>
2013-11-24 11:45:50 +0100

    archive now supports previous and next

--- controller/view/archive.controller.php
+++ controller/view/archive.controller.php
@@ -2,9 +2,25 @@
 
 $DATA['page']['title'] = single_cat_title('', false);
 
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
-		$DATA['posts'][] = new WPOO_Post( $post );
-	}
+the_post();
+$DATA['page'] = new WPOO_Post($post);
+
+$posts = query_posts('posts_per_page=12&paged='.$paged);
+
+while(have_posts()) {
+   the_post();
+   $DATA['posts'][] = new WPOO_Post($post); 
 }
+
+$npl = get_next_posts_link();
+if($npl) {
+    $npl = explode('"',$npl); 
+    $DATA['nextpost']=$npl[1];
+}
+
+$ppl = get_previous_posts_link();
+if($ppl) {
+    $ppl = explode('"',$ppl); 
+    $DATA['prevpost']=$ppl[1];
+}
+wp_reset_postdata(); 
