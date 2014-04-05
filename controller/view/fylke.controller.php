413a511abdfe04f5ee557ae5cafb0a17d29c701b
Marius Mandal <mariusmandal@gmail.com>
2014-04-05 09:26:01 +0200

    auto embed go live

--- controller/view/fylke.controller.php
+++ controller/view/fylke.controller.php
@@ -134,10 +134,24 @@ if( $pl->registered() ) {
 }
 $SEO->description( $description );
 
-if( $VIEW == 'fylke_aktiv' && $_SERVER['REMOTE_ADDR'] == '195.204.59.106' ) {
-	error_reporting( E_ALL );
-	ini_set('display_errors',true);
-	require_once(THEME_PATH.'controller/developer_include.php');
+if( $VIEW == 'fylke_aktiv' ) {
+	$perioder = get_option('ukm_hendelser_perioder');
+	$embedcode = get_option('ukm_live_embedcode');
+	
+	$show_embed = false;
+	
+	if( $embedcode ) {
+		foreach( $perioder as $p ) {
+			if( $p->start < time() && $p->stop > time() ) {
+				$show_embed = true;
+				break;
+			}
+		}
+	}
+	
+	if( $show_embed ) {
+		$DATA['embedcode'] = $embedcode;
+	}
 }
 
 /*
