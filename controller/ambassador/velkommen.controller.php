b91d947bd3655a97d5c5a72bd76014f21302cb5e
Marius Mandal <mariusmandal@gmail.com>
2014-10-20 15:17:10 +0200

    local init FB

--- controller/ambassador/velkommen.controller.php
+++ controller/ambassador/velkommen.controller.php
@@ -3,4 +3,6 @@ require_once('UKM/facebook.class.php');
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-$DATA['ambassador']['loginLink'] = $FACEBOOK_v4->loginLink( 'http://'. UKM_HOSTNAME .'/ambassador/' );
+$FB = new MariusMandalFacebook( UKM_FACE_APP_ID, UKM_FACE_APP_SECRET, 'http://ukm.no/ambassador/' );
+
+$DATA['ambassador']['loginLink'] = $FB->loginLink();
