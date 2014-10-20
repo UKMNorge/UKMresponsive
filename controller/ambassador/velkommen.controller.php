22286b8e105e3843aa0f9e6af5c0b071f3575c9c
Marius Mandal <mariusmandal@gmail.com>
2014-10-20 11:32:29 +0200

    test

--- controller/ambassador/velkommen.controller.php
+++ controller/ambassador/velkommen.controller.php
@@ -1,3 +1,3 @@
 <?php
-#require_once('UKM/facebook.class.php');
+require_once('UKM/facebook.class.php');
 #$DATA['ambassador']['loginLink'] = $FB->loginLink( 'http://'. UKM_HOSTNAME .'/ambassador/' );
