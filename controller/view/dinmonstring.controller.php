c521d112e4ade4bd2df9f8162574ced3d4549ace
Marius Mandal <mariusmandal@gmail.com>
2014-12-04 09:34:23 +0100

    Vis antall påmeldte på "UKM der du bor"
    close #46

--- controller/view/dinmonstring.controller.php
+++ controller/view/dinmonstring.controller.php
@@ -101,3 +101,20 @@ class DinMonstringController {
 
 $controller = new DinMonstringController();
 $DATA = array_merge($DATA, $controller->renderAction());
+
+
+
+$controller = new DinMonstringController();
+$DATA = array_merge($DATA, $controller->renderAction());
+
+require_once('UKM/statistikk.class.php');
+
+$stat = new statistikk();
+$stat->setLand();
+$total = $stat->getTotal(get_option('season'));
+
+$stat = new stdClass();
+$stat->tall 	= $total['persons'];
+$stat->til		= get_option('season');
+
+$DATA['stat_pameldte'] = $stat;
