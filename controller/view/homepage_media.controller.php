cbaae174060c245a33d9cccb5947b104ef23e1fd
Marius Mandal <mariusmandal@gmail.com>
2015-03-04 11:00:00 +0100

    media: oppsett

--- controller/view/homepage_media.controller.php
+++ controller/view/homepage_media.controller.php
@@ -15,7 +15,7 @@ $DATA['blog']['css_extra'][] = 'less/css/festival14.css';
 if( !IN_PRODUCTION_ENVIRONMENT ) {
 	$blocks[] = block_lead( 'lead', UKM_HOSTNAME=='ukm.no' ? 2 : 5);
 
-	$blocks[] = block_image_oob_left('seriost_konsept',
+	$blocks[] = block_image_oob_left('konsept',
 									 UKM_HOSTNAME=='ukm.no' ? 13 : 18,
 									 'https://farm9.staticflickr.com/8451/8021270983_c4e3a3a7f6_c.jpg',
 									 'https://farm9.staticflickr.com/8451/8021270983_c4e3a3a7f6_b.jpg',
@@ -23,20 +23,26 @@ if( !IN_PRODUCTION_ENVIRONMENT ) {
 									 'https://farm9.staticflickr.com/8451/8021270983_639215e84e_h.jpg'
 									);
 
-	$blocks[] = block_lead_center( 'ung_kultur_vises_frem', UKM_HOSTNAME=='ukm.no' ? 11 : 13);
 	
-	$blocks[] = block_lead( 'lead', UKM_HOSTNAME=='ukm.no' ? 2 : 5);
+	$blocks[] = block_lead( 'flerkamera', UKM_HOSTNAME=='ukm.no' ? 25 : 5);
 
-	$blocks[] = block_image_oob_right('backstagefoto',
-									 UKM_HOSTNAME=='ukm.no' ? 17 : 9,
+	$blocks[] = block_lead_center( 'hdbussene', UKM_HOSTNAME=='ukm.no' ? 27 : 13);
+
+	$blocks[] = block_lead( 'videoreportasjer', UKM_HOSTNAME=='ukm.no' ? 29 : 5);
+
+	$blocks[] = block_lead_center( 'studio', UKM_HOSTNAME=='ukm.no' ? 33 : 13);
+
+	$blocks[] = block_image_oob_right('foto',
+									 UKM_HOSTNAME=='ukm.no' ? 35 : 9,
 									 'https://farm4.staticflickr.com/3840/14533175723_732541e923_c.jpg',
 									 'https://farm4.staticflickr.com/3840/14533175723_732541e923_b.jpg',
 									 'https://farm4.staticflickr.com/3840/14533175723_025ebd9485_h.jpg',
 									 'https://farm4.staticflickr.com/3840/14533175723_0bf9a226c8_k.jpg'
 									);
 
+	$blocks[] = block_lead( 'tekst', UKM_HOSTNAME=='ukm.no' ? 37 : 5);
 
-
+/*
 	// ICONS FOR MORE INFO
 	$icons = array();
 	$icons[] = media_icon( '#journalist', 'Tekst', 'http://ico.ukm.no/media_illustrations/tekst.jpg' );
@@ -47,7 +53,7 @@ if( !IN_PRODUCTION_ENVIRONMENT ) {
 	$blocks[] = container_arrowbox( 
 					block_icons( 'icons', $icons, 'Muligheter i UKM Media', 'Redaksjonen er delt inn disse kategoriene - finn hva du kan delta med!' )
 				);
-
+*/
 
 /*	$blocks[] = block_lead( 'lead', 3 );
 
