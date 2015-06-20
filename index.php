fd28ebe7e418752085c83a1a2800dd9b8703b840
danielskogstad <daniel.skogstad@gmail.com>
2015-06-20 09:14:01 +0200

    kun redirect fra ukm.no forside

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 header('Content-Type: text/html; charset=utf-8');
 session_start();
 if( date('m') == 6 && !isset( $_SESSION['festival_redirect'] ) ) {
-	$_SESSION['festival_redirect'] = true;
-	header("Location: http://ukm.no/festivalen/");
-	exit();
+	if(strlen($_SERVER['REQUEST_URI']) < 1) {
+		$_SESSION['festival_redirect'] = true;
+		header("Location: http://ukm.no/festivalen/");
+		exit();
+	}
 }
 
 if( strpos( $_SERVER['HTTP_HOST'], 'ukm.dev' ) !== false || isset($_GET['debug']) ) {
