d33f6c5e73ef2c4b181ed6ef5a7200731aa2c9c3
Marius Mandal <mariusmandal@gmail.com>
2015-11-28 23:28:35 +0100

    Restrukturert side-kontroller

--- controller/view/kommune.controller.php
+++ controller/view/kommune.controller.php
@@ -1,198 +1,304 @@
 <?php
 require_once('UKM/monstring.class.php');
+require_once(THEME_PATH .'class/page.class.php');
 
-$DATA['kommune'] = get_option('kommune');
+class lokalside extends page {
+	var $renderData = array();
+	var $posts_per_page = 7;
+	var $nav = array();
+	var $navOrder = array(	'kontaktpersoner',
+							'ukmtv',
+							'program',
+							'pameldte',
+							'fylke',					
+						);
 
-// HENT ALLE POSTS
-	$DATA['posts'] = array();
-	
-	// LOAD PAGE DATA
-	#the_post();
-	#$DATA['page'] = new WPOO_Post( $post );
-	$DATA['page'] = [];
-	
-// INFO OM MØNSTRINGEN
-    $pl = new monstring( get_option('pl_id') );
-    $monstring = new StdClass();
-    $monstring->pl_id = $pl->get('pl_id');
-    $monstring->navn = str_replace('UKM','',$pl->g('pl_name'));
-    $monstring->starter = $pl->get('pl_start');
-    $monstring->slutter = $pl->get('pl_stop');
-    $monstring->sted = $pl->get('pl_place');
-    $monstring->kommuner = $pl->get('kommuner');
-    $monstring->frist_1 = $pl->get('pl_deadline');
-    $monstring->frist_1_aktiv = $pl->subscribable('pl_deadline');
-    $monstring->frist_2 = $pl->get('pl_deadline2');
-    $monstring->frist_2_aktiv = $pl->subscribable('pl_deadline2');
-    $monstring->bandtypesdetails = $pl->getAllBandTypesDetailedNew();
-    $monstring->starter_tekst = $pl->starter();
 
-    $fpl =  $pl->videresendTil(true);
-    $monstring->fylke = new stdClass();
-    $monstring->fylke->navn = $fpl->g('pl_name');
-    $monstring->fylke->link = $fpl->g('link');
-
-    $DATA['monstring'] = $monstring;
-
-    $kontaktpersoner = $pl->kontakter();
-    if (is_array($kontaktpersoner)) {
-        foreach ( $kontaktpersoner as $kontakt ) {
-            $k           = new stdClass();
-            $k->navn     = $kontakt->get( 'name' );
-            $k->tittel   = $kontakt->get( 'title' );
-            $k->bilde    = $kontakt->get( 'image' );
-            $k->mobil    = $kontakt->get( 'tlf' );
-            $k->epost    = $kontakt->get( 'email' );
-            $k->facebook = $kontakt->get( 'facebook' );
-
-            $kontakter[] = $k;
-
-            $DATA['kontaktpersoner'] = $kontakter;
-        }
-    }
-    
-    
-// HAR UKM-TV-SIDE? (opplastede videoer?)
-	$kategori = ''. $pl->g('pl_name').' '.$pl->g('season');
-	$sql = new SQL("SELECT `tv_id` FROM `ukm_tv_files`
-					WHERE `tv_category` LIKE '#kategori'",
-					array('kategori' => $kategori) );
-	$res = $sql->run();
-	$UKMTV = mysql_num_rows( $res ) > 0 ? $kategori : false;
-
-$posts_per_page = 7;
-// HVILKEN PERIODE ER KOMMUNESIDEN I?      
-        $utenforsesong = mktime(0,0,0,12,1,get_option('season')-1)>time();
-        if (!$pl->registered()) {
-            $VIEW = 'kommune/kommune_ikke_klar';
-        } else if ($utenforsesong) {
-            $VIEW = 'kommune/kommune_pre_pamelding'; 
-        } elseif( time() < $pl->get('pl_deadline') || time() < $pl->get('pl_deadline2') ) {
-            $VIEW = 'kommune/kommune_pamelding';
-            $posts_per_page = 6;
-        } elseif( time() > $pl->get('pl_deadline') && time() < $pl->get('pl_start') ) {
-            $VIEW = 'kommune/kommune_pre';
-        } elseif( time() > $pl->get('pl_start') && time() < $pl->get('pl_stop') ) {
-            $VIEW = 'kommune/kommune_aktiv';
-        } else {
-            $VIEW = 'kommune/kommune_post';
-        }
-    
-$DATA['console']['kommune_view'][] = $VIEW;
-foreach( $monstring as $key => $val ) {
-	if(!is_object( $val ) ) {
-		$DATA['console'][$key][] = @(string) $val;
-	}
-}
-
-$DATA['livelink'] = get_option('ukm_live_link');
-if( $VIEW == 'kommune/kommune_aktiv' ) {
-	$perioder = get_option('ukm_hendelser_perioder');
-	$embedcode = get_option('ukm_live_embedcode');
-	
-	$show_embed = false;
-	
-	if( $embedcode ) {
-		foreach( $perioder as $p ) {
-			if( $p->start < time() && $p->stop > time() ) {
-				$show_embed = true;
+	public function __construct( ) {
+		$this->_loadWPoptions();
+		
+		$this->_viewSelector();
+		switch( $this->view ) {
+			// FØR MØNSTRINGEN ER REGISTRERT
+			case 'ikke_klar':
+				$this->_loadTidligereArAdvarsel();
 				break;
-			}
+			case 'pre_pamelding':
+#				$this->_loadTidligereArAdvarsel();
+				break;
+			case 'pamelding':
+            	$this->posts_per_page = 6;
+				$this->_loadProgram();
+				$this->_loadPameldte();
+				$this->_loadStatistikk();
+				break;
+			case 'pre':
+				$this->_loadProgram();
+				$this->_loadPameldte();
+				$this->_loadStatistikk();
+				break;
+			case 'aktiv':
+				$this->_loadUKMTV();
+				$this->_loadProgram();
+				$this->_loadPameldte();
+				$this->_loadStatistikk();
+				break;
+			case 'post':
+				$this->_loadUKMTV();
+				$this->_loadProgram();
+				$this->_loadPameldte( true );
+				$this->_loadStatistikk();
+				break;
+		}
+		$this->_loadPosts();
+		$this->_baseNav();
+	}
+			
+	/**
+	 * Velg hvilket view som skal vises
+	 *
+	 */
+	private function _viewSelector() {
+		$this->_requireMonstring();
+		$utenforsesong = mktime(0,0,0,12,1,get_option('season')-1)>time();
+		
+        if( !$this->pl->registered() ) {
+            $this->view = 'ikke_klar';
+        } elseif ( $utenforsesong ) {
+            $this->view = 'pre_pamelding'; 
+        } elseif( time() < $this->pl->get('pl_deadline') || time() < $this->pl->get('pl_deadline2') ) {
+            $this->view = 'pamelding';
+        } elseif( time() > $this->pl->get('pl_deadline') && time() < $this->pl->get('pl_start') ) {
+            $this->view = 'pre';
+        } elseif( time() > $this->pl->get('pl_start') && time() < $this->pl->get('pl_stop') ) {
+            $this->view = 'aktiv';
+        } else {
+            $this->view = 'post';
+        }
+	}
+	
+	/**
+	 * Vis advarsel-boks "leter du etter tidligere år?
+	 *
+	 */
+	private function _loadTidligereArAdvarsel() {
+		if( 8 < date('m') && date('m') < 12 ) {
+			$this->renderData['tidligereArAdvarsel'] = 'tidligereArAdvarsel';	
+			$this->tidligereArAdvarsel = true;
 		}
 	}
 	
-	if( $show_embed ) {
-		$DATA['embedcode'] = $embedcode;
+	/**
+	 * Last inn informasjon om mønstringen og fylkesmønstringen
+	 * 
+	 */
+	private function _loadMonstring() {
+		$this->renderData['monstring'] = 'monstring';	
+
+	    $pl = new monstring( get_option('pl_id') );
+		$this->pl = $pl;
+	    $this->monstring = new StdClass();
+	    $this->monstring->pl_id = $pl->get('pl_id');
+	    $this->monstring->navn = str_replace('UKM','',$pl->g('pl_name'));
+	    $this->monstring->starter = $pl->get('pl_start');
+	    $this->monstring->slutter = $pl->get('pl_stop');
+	    $this->monstring->sted = $pl->get('pl_place');
+	    $this->monstring->kommuner = $pl->get('kommuner');
+	    $this->monstring->frist_1 = $pl->get('pl_deadline');
+	    $this->monstring->frist_1_aktiv = $pl->subscribable('pl_deadline');
+	    $this->monstring->frist_2 = $pl->get('pl_deadline2');
+	    $this->monstring->frist_2_aktiv = $pl->subscribable('pl_deadline2');
+	    $this->monstring->bandtypesdetails = $pl->getAllBandTypesDetailedNew();
+	    $this->monstring->starter_tekst = $pl->starter();
+	
+	    $fpl =  $pl->videresendTil(true);
+	    $this->videresend_til = $fpl;
+	    
+	    $this->monstring->fylke = new stdClass();
+	    $this->monstring->fylke->navn = $fpl->g('pl_name');
+	    $this->monstring->fylke->link = $fpl->g('link');
+	}
+	
+	/**
+	 * Last inn kontaktpersoner som tilhører mønstringen
+	 */
+	private function _loadKontaktpersoner() {
+		$this->renderData['kontaktpersoner'] = 'kontaktpersoner';	
+		$this->kontaktpersoner = array();
+		$this->_requireMonstring();
+				
+	    $kontaktpersoner = $pl->kontakter();
+	    if (is_array($kontaktpersoner)) {
+	        foreach ( $kontaktpersoner as $kontakt ) {
+	            $k           = new stdClass();
+	            $k->navn     = $kontakt->get( 'name' );
+	            $k->tittel   = $kontakt->get( 'title' );
+	            $k->bilde    = $kontakt->get( 'image' );
+	            $k->mobil    = $kontakt->get( 'tlf' );
+	            $k->epost    = $kontakt->get( 'email' );
+	            $k->facebook = $kontakt->get( 'facebook' );
+	
+	            $kontakter[] = $k;
+	
+	            $this->kontaktpersoner = $kontakter;
+	        }
+	    }
+	}
+	
+	/**
+	 * Last inn statistikk for mønstringen
+	 *
+	 **/
+	private function _loadStatistikk() {
+		require_once('UKM/statistikk.class.php');
+		$this->_requireMonstring();
+		$this->renderData['stat_pameldte'] = 'stat_pameldte';		
+
+		$stat = $this->pl->statistikk();										  
+		$total = $stat->getTotal($this->pl->get('season'));
+
+		$stat = new stdClass();
+		$stat->tall 	= $total['persons'];
+		$stat->til		= $this->monstring->navn;
+		$this->stat_pameldte = $stat; 
+	}
+	
+	/**
+	 * Bruker siden live-modulen? vis lenke / embedkode
+	 *
+	 */	
+	private function _liveLink() {
+		$this->renderData['livelink'] = 'embedcode';		
+
+		$this->livelink = get_option('ukm_live_link');
+		$perioder 		= get_option('ukm_hendelser_perioder');
+		$embedcode 		= get_option('ukm_live_embedcode');
+			
+		$show_embed = false;
+		if( $embedcode ) {
+			foreach( $perioder as $p ) {
+				if( $p->start < time() && $p->stop > time() ) {
+					$show_embed = true;
+					break;
+				}
+			}
+		}
+			
+		if( $show_embed ) {
+			$DATA['embedcode'] = $embedcode;
+		}
+	}
+
+
+	/** 
+	 * Hvis mønstringen har UKM-TV, vis menyelement
+	 *
+	 */
+	private function _loadUKMTV() {
+		$this->_requireMonstring();
+		require_once('UKM/tv_files.class.php');
+		
+		// Hent filer fra mønstringen
+		$tv_files = new tv_files( 'place', $this->pl->get('pl_id') );
+		$tv_files->limit(1);
+		$tv_files->fetch();
+		
+		$this->ukmtv = $tv_files->num_videos > 0;
+		
+		// Registrer meny
+		if( $this->ukmtv ) {
+			$nav = new stdClass();
+			$nav->url 			= '//tv.ukm.no';
+			$nav->title			= 'Film';
+			$nav->icon 			= 'ukmtv_black';
+			$nav->description	= 'Film fra '. $this->pl->get('pl_name').' i UKM-TV';
+			$nav->target		= '_blank';
+			
+			$this->registerNav( 'ukmtv', $nav );
+	    }
+	}
+	
+	/**
+	 * Hvis mønstringen har program, vis menyelement
+	 *
+	 */	
+	private function _loadProgram() {
+		$this->_requireMonstring();
+		
+		if($this->pl->har_program() ) {
+			$nav = new stdClass();
+			$nav->url			= 'program/';
+			$nav->title 		= 'Program';
+			$nav->icon 			= 'calendar';
+			$nav->description	= 'Se program for lokalmønstringen';
+			
+			$this->registerNav( 'program', $nav );
+		}
+	}
+	
+	/**
+	 * Hvis mønstringen har påmeldte, vis menyelement
+	 *
+	 */	
+	private function _loadPameldte( $in_the_past = false) {
+		$this->_requireMonstring();
+		
+		$innslag = $this->pl->innslag();
+		if( sizeof( $innslag ) > 0 ) {
+			$nav = new stdClass();
+			$nav->url 			= 'pameldte/';
+			$nav->title			= 'Hvem '. ($in_the_past ? 'deltok' : 'deltar' ) .'?';
+			$nav->icon			= 'people';
+			$nav->description	= 'Se alle som '. ($in_the_past ? 'deltok' : 'deltar' ) .'.';
+			$this->registerNav( 'pameldte', $nav );
+		    
+		}
+	}	
+	
+	/**
+	 * Basis-navigasjon (er med på alle sider)
+	 *
+	 */
+	private function _baseNav() {
+		$this->_requireMonstring();
+		
+		$nav = new stdClass();
+		$nav->url 			= '#kontaktpersoner';
+		$nav->title			= 'Kontaktpersoner';
+		$nav->icon 			= 'i';
+		$nav->description 	= 'Har du spørsmål om UKM i '. $this->pl->get('pl_name').'? Disse kan hjelpe!';
+		$nav->id 			= 'show_kontaktpersoner';
+		$this->registerNav( 'kontaktpersoner', $nav );
+		
+		
+		$nav = new stdClass();
+		$nav->url 			= $this->monstring->fylke->link;
+		$nav->title 		= 'UKM '. $this->monstring->fylke->navn;
+		$nav->icon 			= 'maps';
+		$nav->description 	= 'Info om UKM i '. $this->monstring->fylke->navn;
+		$this->registerNav( 'fylke', $nav );
+	}
+
+	/**
+	 * Last inn mønstringen hvis ikke allerede gjort
+	 *
+	 */
+	protected function _requireMonstring() {
+		if( !isset( $this->pl ) ) {
+			$this->_loadMonstring();
+		}
 	}
 }
 
-// PÅMELDINGSIKONER
+
+
+
 $DATA = array_merge($DATA, $pl->pameldingsikoner());
-		
-		
-	// LOAD POSTS
-    $paged = (get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-    
-    $posts = query_posts('posts_per_page='.$posts_per_page.'&paged='.$paged);
-    while(have_posts()) {
-       the_post();
-       $DATA['posts'][] = new WPOO_Post($post); 
-    }
-    
-    $npl = get_next_posts_link();
-    if($npl) {
-        $npl = explode('"',get_next_posts_link()); 
-        $DATA['nextpost']=$npl[1];
-    }
-    $ppl = get_previous_posts_link();
-    if($ppl) {
-        $ppl = explode('"',$ppl); 
-        $DATA['prevpost']=$ppl[1];
-    }
-
-// PAGE NAV
-
-/*
-        $DATA['page_nav'][] = (object) array( 'url' => 'bilder/',
-                                              'title' => 'Bilder',
-                                              'icon'  => 'kamera',
-                                              'description' => 'Bilder fra '. $pl->get('pl_name').''
-                                          );
-*/
-if( $UKMTV ) {
-    $DATA['page_nav'][] = (object) array( 'url'			=> '//tv.ukm.no',
-                                          'title'		=> 'Film',
-                                          'icon' 		=> 'ukmtv_black',
-                                          'description' => 'Film fra '. $pl->get('pl_name').' i UKM-TV',
-                                          'target'		=> '_blank'
-                                      );
-}
-
-if( in_array( $VIEW, array('kommune/kommune_pre','kommune/kommune_aktiv','kommune/kommune_post')) || $pl->har_program() ) {
-$DATA['page_nav'][] = (object) array( 'url' 			=> 'program/',
-									   'title'		 	=> 'Program',
-									   'icon'			=> 'calendar',
-                                        'description'	=> 'Se program for lokalmønstringen'
-                                       );
-}
-$innslag = $pl->innslag();
-if( sizeof( $innslag ) > 0 && $VIEW == 'kommune/kommune_post' ) {
-    $DATA['page_nav'][] = (object) array( 'url'			=> 'pameldte/',
-                                          'title'		=> 'Hvem deltok?',
-                                          'icon'		=> 'people',
-                                          'description' => 'Se alle som deltok på lokalmønstringen.'
-                                      );
-} elseif( sizeof( $innslag ) > 0 ) {
-    $DATA['page_nav'][] = (object) array( 'url'			=> 'pameldte/',
-                                          'title'		=> 'Hvem deltar?',
-                                          'icon'		=> 'people',
-                                          'description' => 'Se alle som deltar på lokalmønstringen.'
-                                      );
-}
 
 
-$DATA['page_nav'][] = (object) array( 'url' 			=> '#kontaktpersoner',
-                                      'title' 			=> 'Kontaktpersoner',
-                                      'icon'  			=> 'i',
-                                      'description' 	=> 'Har du spørsmål om UKM i '. $pl->get('pl_name').'? Disse kan hjelpe!',
-										   'id'			=> 'show_kontaktpersoner'
-                                  );
-$DATA['page_nav'][] = (object) array( 'url' 			=> $monstring->fylke->link,
-                                      'title' 			=> 'UKM '. $monstring->fylke->navn,
-                                      'icon'  			=> 'maps',
-                                      'description' 	=> 'Info om UKM i '. $monstring->fylke->navn
-                                  );
-$description = 'Nyheter og informasjon om UKM '. $monstring->navn;
-if( $pl->registered() ) {
-	$description .= ' - '. $monstring->starter_tekst.', '. $monstring->sted;
-}
-$SEO->description( $description );
 
 
-require_once('UKM/statistikk.class.php');
-$stat = $pl->statistikk();										  
-$total = $stat->getTotal($pl->get('season'));
-$stat = new stdClass();
-$stat->tall 	= $total['persons'];
-$stat->til		= $monstring->navn;
-$DATA['stat_pameldte'] = $stat; 
+$lokalside = new lokalside();
+$VIEW = $lokalside->getView();
+$DATA = array_merge( $DATA, $lokalside->renderData() );
+$SEO = $lokalside->SEO( $SEO );
