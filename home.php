27d6b39de18cc5babc375c02b9f37643195ed9ea
Marius Mandal <mariusmandal@gmail.com>
2017-02-01 15:27:15 +0100

    wp-footer

--- home.php
+++ home.php
@@ -12,3 +12,8 @@ $WP_TWIG_DATA['page'] = $page;
 
 #echo Sitemap::getSections();
 echo WP_TWIG::render( 'Page/home_norge', $WP_TWIG_DATA );
+
+	wp_footer();
+	if(is_user_logged_in() ) {
+		echo '<style>body {margin-top: 33px;}</style>';
+	}
