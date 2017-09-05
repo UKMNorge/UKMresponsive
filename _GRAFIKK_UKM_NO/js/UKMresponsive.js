3860b9e8f4ebce3a707052073760e65ab18ce3a4
Marius Mandal <mariusmandal@gmail.com>
2017-09-05 20:39:47 +0200

    Added autoshrink

--- _GRAFIKK_UKM_NO/js/UKMresponsive.js
+++ _GRAFIKK_UKM_NO/js/UKMresponsive.js
@@ -1,3 +1,6 @@
+/**
+ * MAIN MENU BUTTON TOGGLE
+**/
 jQuery( document ).on('click', '#main_menu_button', function(e){
 	e.preventDefault();
 	
@@ -22,24 +25,35 @@ jQuery( document ).on('click', '#main_menu_button', function(e){
 	}
 });
 
-/* UKM-toggle
-KNAPPER FOR Å VISE KREVER:
- - data-target="id-på-div-som-skal-toggles"
- - class="UKMtoggleShow id-på-div-som-skal-toggles
- 
-KNAPPER FOR Å SKJULE KREVER:
- - data-target="id-på-div-som-skal-toggles"
- - class="UKMtoggleShow id-på-div-som-skal-toggles
 
-CONTAINER SOM SKAL TOGGLES KREVER:
- - class="UKMtoggleContent"
- - id="id-på-div-som-skal-toggles"
-*/
+/** 
+ * AUTOSHRINK FOR ALLE .autoshrink
+ * OBS: kan kreve mye av nettleseren, så spar bruken!
+**/	
+$(document).ready( function() {
+	$('.autoshrink').autoshrink();
+});
+
+
+/** 
+ * UKM-toggle
+ *	KNAPPER FOR Å VISE KREVER:
+ *	 - data-target="id-på-div-som-skal-toggles"
+ *	 - class="UKMtoggleShow id-på-div-som-skal-toggles
+ *	 
+ *	KNAPPER FOR Å SKJULE KREVER:
+ *	 - data-target="id-på-div-som-skal-toggles"
+ *	 - class="UKMtoggleShow id-på-div-som-skal-toggles
+ *	
+ *	CONTAINER SOM SKAL TOGGLES KREVER:
+ *	 - class="UKMtoggleContent"
+ *	 - id="id-på-div-som-skal-toggles"
+**/
 $(document).on('click', '.UKMtoggleShow', function(e){
 	e.preventDefault();
 	
 	var target = $(this).attr('data-target');
-	$('#' + target + '.UKMtoggleContent').slideDown();
+	$('#' + target + '.UKMtoggleContent').slideDown(function(){$(document).trigger('UKMtoggleShow#'+ target);});
 	$('.' + target + '.UKMtoggleShow').hide();
 	$('.' + target + '.UKMtoggleHide').fadeIn();
 });
@@ -48,7 +62,8 @@ $(document).on('click', '.UKMtoggleHide', function(e){
 	e.preventDefault();
 	
 	var target = $(this).attr('data-target');
-	$('#' + target + '.UKMtoggleContent').slideUp();
+	$('#' + target + '.UKMtoggleContent').slideUp(function(){$(document).trigger('UKMtoggleHide#'+ target);});
 	$('.' + target + '.UKMtoggleHide').hide();
 	$('.' + target + '.UKMtoggleShow').fadeIn();
+	$(document).trigger('UKMtoggleHide#'+ target);
 });
