96f0e48529f54965aeeed87e7f6964f545721651
Marius Mandal <mariusmandal@gmail.com>
2017-09-21 13:29:49 +0200

    Bruker riktig meta for å hente MenuID
    Related: https://github.com/UKMNorge/UKMwp_admin-tweaks/pull/18

--- UKMNorge/Wordpress/Controller/menu.controller.php
+++ UKMNorge/Wordpress/Controller/menu.controller.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$meny = wp_get_nav_menu_object( $WP_TWIG_DATA['page']->getPage()->meta->temp_nav_id );	
-	$WP_TWIG_DATA['meny'] = wp_get_nav_menu_items( $WP_TWIG_DATA['page']->getPage()->meta->temp_nav_id );
+	$meny = wp_get_nav_menu_object( $WP_TWIG_DATA['page']->getPage()->meta->UKM_nav_menu );	
+	$WP_TWIG_DATA['meny'] = wp_get_nav_menu_items( $WP_TWIG_DATA['page']->getPage()->meta->UKM_nav_menu );
 	
 #	var_dump( $WP_TWIG_DATA['meny']  );
