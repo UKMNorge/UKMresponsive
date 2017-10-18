8ff42a0c89f479cfb71829230385d82b8c5b60ff
Marius Mandal <mariusmandal@gmail.com>
2017-10-18 17:19:55 +0200

    Fjernet den gamle koden

--- _OLD/wp-admin/layout.php
+++
@@ -1,5 +0,0 @@
-<?php
-$TWIGdata = array();
-$TWIGdata['customizable'] =  !in_array(get_option('site_type'), array('kommune','fylke','land'));
-$TWIGdata['site_type'] = get_option('site_type');
-echo TWIG('content.twig.html', $TWIGdata, dirname(__FILE__));
