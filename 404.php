8c4a71743fcbee2bbf4c80f24a93d49933c93b93
Marius Mandal <mariusmandal@gmail.com>
2018-02-20 20:37:37 +0100

    Print footer også på 404

--- 404.php
+++ 404.php
@@ -25,6 +25,11 @@ if( isset( $WP_TWIG_DATA['page']->getPage()->meta->UKMviseng ) ) {
 // RENDER
 echo WP_TWIG::render( '404/404', $WP_TWIG_DATA );
 
+wp_footer();
+if(is_user_logged_in() ) {
+	echo '<style>body {margin-top: 33px;} @media (max-width:782px) {body {margin-top: 48px;}}</style>';
+}
+
 if( WP_ENV == 'dev' ) {
 	echo '<script language="javascript">console.debug("'.basename(__FILE__).'");</script>';
 }
