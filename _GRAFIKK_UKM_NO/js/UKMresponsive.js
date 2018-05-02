9d68678fd2c13ea054527e529a2989acf444c9ba
Marius Mandal <mariusmandal@gmail.com>
2018-05-02 09:15:49 +0200

    Inline-visning av deltakere

--- _GRAFIKK_UKM_NO/js/UKMresponsive.js
+++ _GRAFIKK_UKM_NO/js/UKMresponsive.js
@@ -176,8 +176,58 @@ $(document).on('click','#saveAsMine', function(e){
 /**
  * DELTAKERE-SIDEN
 **/
-$(document).on('click', '.innslagCard', function(e){
-	window.location.href = $(this).find('.name > a').attr('href');
+$( document ).on('visInnslag', function( e, innslag ){
+	innslag.attr('data-visning', 'synlig');
+	var dataContainer = innslag.find('.innslagData');
+	var buttonContainer = innslag.find('.innslagCancel');
+	var header = innslag.find('.header');
+	
+	header.slideUp();
+	$.post(
+			window.location.href + '/'+ innslag.attr('data-id') +'/',
+			{
+				singleMode: "true"
+			},
+			function( response ) {
+				dataContainer.html( response );
+			}
+		);
+	dataContainer.slideDown();
+	buttonContainer.fadeIn();
+});
+
+$( document ).on('skjulInnslag', function( e, innslag ){
+	innslag.attr('data-visning', 'skjult');
+	var dataContainer = innslag.find('.innslagData');
+	var buttonContainer = innslag.find('.innslagCancel');
+	var header = innslag.find('.header');
+	
+	header.slideDown();
+	dataContainer.slideUp(
+		400,
+		function(){
+			dataContainer.html('<div class="center">Vennligst vent, laster inn...</div>');
+		}
+	);
+	buttonContainer.fadeOut();
+});
+
+$( document ).on('click', '.innslagCard .header', function( e ){
+	e.preventDefault();
+	
+	innslag = $( this ).parents('.innslagCard');
+	if( innslag.attr('data-visning') == 'synlig' ) {
+		$( document ).trigger('skjulInnslag', [ innslag ] );
+	} else {
+		$( document ).trigger('visInnslag', [ innslag ] );
+	}
+	
+	return false;
+});
+
+$( document ).on('click', '.hideInnslag', function(){
+	var innslag = $(this).parents('.innslagCard');
+	$( document ).trigger('skjulInnslag', [ innslag ] );
 });
 /**
  * AOS - Animate On Scroll
