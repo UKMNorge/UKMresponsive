447769851b964c5c2b667613ec0d938c71c8543d
ukmno <ukmno@ukm.no>
2018-05-02 09:31:52 +0200

    Media sier hei igjen

--- front-page.php
+++ front-page.php
@@ -46,6 +46,14 @@ switch( get_option('site_type') ) {
 		$view_template = 'Samtykke/view';
 		require_once('UKMNorge/Wordpress/Controller/samtykke.controller.php');
 		break;
+	case 'media':
+		$view_template = 'Media/home';
+		require_once('UKMNorge/Wordpress/Controller/menu.controller.php');
+		$WP_TWIG_DATA['HEADER']->background->url = '//ukm.no/media/files/2018/05/2016-06-27-14.57.29-1800x1350.jpg';
+		$WP_TWIG_DATA['HEADER']->background->position = 'top';
+		$WP_TWIG_DATA['HEADER']->slogan = 'UKM sin medieavdeling - av og for ungdom';
+		$WP_TWIG_DATA['HEADER']->button->background = 'rgba(242, 109, 21, 0.44)';
+		break;
 	case 'gdpr':
 	case 'site':
 		$view_template = 'Page/fullpage';
