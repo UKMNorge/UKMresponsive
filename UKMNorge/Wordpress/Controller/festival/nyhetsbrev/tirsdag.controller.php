54631e7f5e2515b2db4b94ce24255011d2e8fc34
Asgeir Stavik Hustad <asgeirsh@ukmmedia.no>
2018-06-25 15:51:31 +0000

    Nyhetsbrev

--- UKMNorge/Wordpress/Controller/festival/nyhetsbrev/tirsdag.controller.php
+++ UKMNorge/Wordpress/Controller/festival/nyhetsbrev/tirsdag.controller.php
@@ -2,7 +2,7 @@
 use UKMNorge\DesignBundle\Utils\SEO;
 use UKMNorge\DesignBundle\Utils\SEOImage;
 
-$WP_TWIG_DATA['HEADER']->background->url = 'http://ukm.dev/festivalen/wp-content/uploads/sites/60/2018/06/Screen-Shot-2018-06-19-at-21.11.18.png';#'//grafikk.ukm.no/UKMresponsive/img/festival/forsidebilde.jpg';
+#$WP_TWIG_DATA['HEADER']->background->url = 'http://ukm.dev/festivalen/wp-content/uploads/sites/60/2018/06/Screen-Shot-2018-06-19-at-21.11.18.png';#'//grafikk.ukm.no/UKMresponsive/img/festival/forsidebilde.jpg';
 $WP_TWIG_DATA['HEADER']->hideSection = true;
 $WP_TWIG_DATA['HEADER']->hideLogo = true;
 
