c4391bf24358b56fc0e6937e5764f00c0a2cc817
Marius Mandal <mariusmandal@gmail.com>
2018-11-05 14:07:26 +0100

    prøv å unngå å overskrive home-var

--- functions.php
+++ functions.php
@@ -93,7 +93,7 @@ WP_TWIG::setTemplateDir( PATH_DESIGNBUNDLE .'Resources/views/' );
 if( !isset( $_ENV['HOME'] ) && UKM_HOSTNAME == 'ukm.no' ) {
 	$_ENV['HOME'] = '/home/ukmno/';	
 }
-if( WP_ENV == 'dev' ) {
+elseif( WP_ENV == 'dev' ) {
 	$_ENV['HOME'] = '/temp';
 }
 WP_TWIG::setCacheDir( $_ENV['HOME'].'/cache/ukmresponsive/');
