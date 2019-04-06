0f89514035f94f70f889972f01f2e74a01a47b5f
Marius Mandal <mariusmandal@gmail.com>
2019-04-06 21:14:42 +0200

    Vis kun én forestilling

--- UKMNorge/Wordpress/Controller/monstring/bilder.controller.php
+++ UKMNorge/Wordpress/Controller/monstring/bilder.controller.php
@@ -1,3 +1,7 @@
 <?php
-	
+
+if( isset( $_GET['hendelse'] ) ) {
+	$WP_TWIG_DATA['vis'] = $_GET['hendelse'];
+}
+$WP_TWIG_DATA['visAlle'] = !isset( $_GET['hendelse'] );
 $WP_TWIG_DATA['monstring'] = new monstring_v2( get_option('pl_id') );
