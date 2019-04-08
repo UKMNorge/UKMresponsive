01e03cc84a89200c832776f771ff4c9f7aea451c
Marius Mandal <mariusmandal@gmail.com>
2019-04-08 12:07:12 +0200

    Riktig fullpage på forsiden

--- _GRAFIKK_UKM_NO/js/UKMfullpage.js
+++ _GRAFIKK_UKM_NO/js/UKMfullpage.js
@@ -1,5 +1,7 @@
-$(document).ready( function(){	
-	$('.fullpage').each( function(){
+// @codekit-prepend 'fullpage.js'
+
+function UKMfullpage( id, anchors ) {
+	$(id).find('.fullpage').each( function(){
 		var fullpage = $(this);
 		var image = new Image();
 		if( fullpage.attr('data-photo-background') == 'false' ) {
@@ -15,11 +17,11 @@ $(document).ready( function(){
 			image.src = $(this).attr('data-photo-background');
 		}
 	});
-	
-	new fullpage('#fullpage', {
-		anchors:['start', 'om', 'finn'],
+
+	new fullpage( id, {
+		anchors: anchors,
 		licenseKey: 'D123E63D-4F5C490D-92314334-48693F2A',
 		navigation: true,
-		scrollOverflow: true,
+		scrollOverflow: true
 	});
-});
+}
