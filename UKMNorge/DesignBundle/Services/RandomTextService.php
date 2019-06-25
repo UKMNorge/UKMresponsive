d12807dee93c5dfce4e23872423ad87f2c10fa51
Asgeir Stavik Hustad <askhus@online.no>
2019-06-25 19:59:55 +0200

    Commit RandomTextService

---
+++ UKMNorge/DesignBundle/Services/RandomTextService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace UKMNorge\DesignBundle\Services;
+
+/**
+ * Leverer en random streng til nyhetsbrevet.
+ *
+ */
+class RandomTextService {	
+	
+ 	var $navn = [
+		"Magnus",
+		"Jonas",
+		"Thomas",
+		"Emma",
+		"Julie",
+		"Marte",
+		"Martin",
+		"Anna",
+		"Mari",
+		"Stine",
+		"Sara"
+	];
+
+	var $action = [
+		"juletreklatring (førstemann til stjerna)",
+		"pepperkakebaking",
+		"kurs i brillepussing",
+		"snømåke-workshop",
+		"kaffetrakte-kurs",
+		"birøkter-kurs",
+		"ballongblåse-workshop"
+	];
+
+	var $where = [
+		"i bakgården",
+		"på Kimen",
+		"i 1001 natt",
+		"i bestemorkroken",
+		"på soverommet"
+	];
+
+	public static function getText() {
+		$navnRand = rand(0,size($navn)-1);
+		$actionRand = rand(0, size($action)-1);
+		$where = rand(0, size($where)-1);
+		return $navn[$navnRand] + ' ' + $action[$actionRand] + ' ' + $where[$whereRand];
+	}
+	
+	
+}
