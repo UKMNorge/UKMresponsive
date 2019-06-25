28c81bdec1e109655189ed0733d9a030d11c904e
Thomas Hovstad <thomas.hovstad@outlook.com>
2019-06-25 20:31:28 +0200

    nyhetsbrev publish

--- UKMNorge/Wordpress/Controller/festival/nyhetsbrev.controller.php
+++ UKMNorge/Wordpress/Controller/festival/nyhetsbrev.controller.php
@@ -1,6 +1,7 @@
 <?php
 use UKMNorge\DesignBundle\Utils\SEO;
 use UKMNorge\DesignBundle\Utils\SEOImage;
+use UKMNorge\DesignBundle\Services\RandomTextService;
 
 #$WP_TWIG_DATA['HEADER']->background->url = 'http://ukm.dev/festivalen/wp-content/uploads/sites/60/2018/06/Screen-Shot-2018-06-19-at-21.11.18.png';#'//grafikk.ukm.no/UKMresponsive/img/festival/forsidebilde.jpg';
 $WP_TWIG_DATA['HEADER']->hideSection = true;
@@ -9,12 +10,8 @@ $WP_TWIG_DATA['HEADER']->hideLogo = true;
 
 $current_date = $WP_TWIG_DATA['page']->getPage()->meta->dato;
 
+$WP_TWIG_DATA['currentText'] = RandomTextService::getText();
 
 
-
-
-require_once('UKM/tv.class.php');
-// Tøffelvideo
-$WP_TWIG_DATA['mandag_toffel'] = new TV(14744);
 
 $view_template = 'Festival/Nyhetsbrev/2019_'. $current_date;
